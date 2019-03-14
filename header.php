bd3a587d8b43a4f4170e426070b6cb57de01bf66
mohit2708 <42208944+mohit2708@users.noreply.github.com>
2019-03-14 23:46:04 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,2 +1,16 @@
 <title><?php if (is_front_page() ) { bloginfo('name'); } else { wp_title(''); ?> | <?php bloginfo('name'); } ?></title>
 logo: <?php the_custom_logo(); ?>
+<?php
+   wp_nav_menu([
+     'menu'            => 'header-menu',
+     'theme_location'  => 'top',
+     'container'       => 'div',
+     'container_id'    => 'bs4navbar',
+     'container_class' => 'collapse navbar-collapse',
+     'menu_id'         => false,
+     'menu_class'      => 'navbar-nav',
+     'depth'           => 2,
+     'fallback_cb'     => 'bs4navwalker::fallback',
+     'walker'          => new bs4navwalker()
+   ]);
+   ?>
