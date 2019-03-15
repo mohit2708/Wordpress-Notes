c41720883c30f29703065a6cda60bb9bf5cbb310
mohit2708 <42208944+mohit2708@users.noreply.github.com>
2019-03-15 15:08:42 +0530

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,1 +1,6 @@
+<?php	while ( have_posts() ) : the_post(); ?>
+Title: <?php the_archive_title(); ?>
+Description: <?php echo category_description(); ?>(OR)<?php the_archive_description(); ?>
 
+
+<?php endwhile; ?>
