4ce824bfa124ef7421e9f0f5daa27add46b5c0e8
mohit2708 <42208944+mohit2708@users.noreply.github.com>
2019-03-15 15:24:04 +0530

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,5 +1,6 @@
 <?php	while ( have_posts() ) : the_post(); ?>
 Title: <?php the_archive_title(); ?>
+Title:  <?php echo str_replace("Category: ", "", get_the_archive_title()); ?> 
 Description: <?php echo category_description(); ?>(OR)<?php the_archive_description(); ?>
 
 
