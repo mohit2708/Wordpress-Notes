d3110627616391a27e5b26b527ec8a18132d332a
mohit2708 <42208944+mohit2708@users.noreply.github.com>
2019-03-15 15:42:08 +0530

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,7 +1,9 @@
-<?php	while ( have_posts() ) : the_post(); ?>
 Title: <?php the_archive_title(); ?>
 Title:  <?php echo str_replace("Category: ", "", get_the_archive_title()); ?> 
 Description: <?php echo category_description(); ?>(OR)<?php the_archive_description(); ?>
 
-
-<?php endwhile; ?>
+<?php	while ( have_posts() ) : the_post(); ?>
+<?php the_title(); ?>
+<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ));?>
+<?php the_content(); ?>
+<?php endwhile; wp_reset_postdata(); ?>
