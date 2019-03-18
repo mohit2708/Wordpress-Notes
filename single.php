3f7bce4ac4fd12f02964fc5c84ed47c3be88346d
Mohit Saxena <42208944+mohit2708@users.noreply.github.com>
2019-03-18 13:58:31 +0530

    Update single.php

--- single.php
+++ single.php
@@ -4,9 +4,14 @@ Content: <p><?php the_content(); ?></p>
 Author: <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php echo get_the_author(); ?></a>
 Date: <?php echo get_the_time( 'F j, Y', $post->ID ) ?>
 Days Ago: <?php echo human_time_diff(get_the_time('U'), current_time('timestamp')) . ' ago'; ?>
+Time: <?php the_time(); ?>
+Tag: <?php the_tags(); ?>
+Expert: <?php the_excerpt(); ?>
 Comment: <?php comments_popup_link('No Comments ', '1 Comment ', '% Comments '); ?>
 Image: <?php echo the_post_thumbnail( array(300, 300, true) ); ?>
 Image: <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ));?>" /> 
+Next Link: <?php next_posts_link(); ?>
+Previous Link: <?php previous_posts_link(); ?>
 <?php endwhile;	?>
 <!------------------------------- Search Bar ---------------------------->
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
