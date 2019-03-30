843707c5e879c567269da0141e230624c49058b5
Mohit Saxena <42208944+mohit2708@users.noreply.github.com>
2019-03-30 16:19:40 +0530

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,14 +1,9 @@
 
 <?php get_header(); ?>
-
 <div class="container">
-<img src="http://swadeshiayurvedic.com/yogmantrana/wp-content/uploads/2019/03/404.jpg" style="">
-
 <h1>Oops! That page can&rsquo;t be found.</h1>
 <p>It looks like nothing was found at this location</p>
 </div>
-
-
 <script>
          setTimeout(function(){
             window.location.href = '<?php echo site_url('/'); ?>';
