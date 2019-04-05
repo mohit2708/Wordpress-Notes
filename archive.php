655f2c352905e2ea73d67fe90e6335c5c606ff7a
Mohit Saxena <42208944+mohit2708@users.noreply.github.com>
2019-04-05 11:47:28 +0530

    Update archive.php

--- archive.php
+++ archive.php
@@ -1,10 +1,12 @@
-Title: <?php the_archive_title(); ?>
-Title:  <?php echo str_replace("Category: ", "", get_the_archive_title()); ?> 
-Description: <?php echo category_description(); ?>(OR)<?php the_archive_description(); ?>
+<?php get_header(); ?>
+  Title: <?php the_archive_title(); ?>
+  Title:  <?php echo str_replace("Category: ", "", get_the_archive_title()); ?> 
+  Description: <?php echo category_description(); ?>(OR)<?php the_archive_description(); ?>
 
-<?php	while ( have_posts() ) : the_post(); ?>
-<?php the_title(); ?>
-<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ));?>
-<?php the_content(); ?>/<?php echo substr($post->post_content, 0, 150);  ?>
-<a href="<?php echo get_permalink( $post->ID ); ?>" class="btn btn-small">Register</a>
-<?php endwhile; wp_reset_postdata(); ?>
+  <?php	while ( have_posts() ) : the_post(); ?>
+  <?php the_title(); ?>
+  <?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ));?>
+  <?php the_content(); ?>/<?php echo substr($post->post_content, 0, 150);  ?>
+  <a href="<?php echo get_permalink( $post->ID ); ?>" class="btn btn-small">Register</a>
+  <?php endwhile; wp_reset_postdata(); ?>
+<?php get_footer(); ?>
